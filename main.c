9ffa2142dc54a5607575cb9b66d0acf78b41ab6a
Lysandr <psl58@cornell.edu>
2017-05-13 02:40:10 -0400

    testo

--- main.c
+++ main.c
@@ -294,12 +294,33 @@ void main_loop(){
     esc3.pulsewidth_us(esc_3);    // set the initial pulsewidth to 800 us just to keep them on
     esc4.pulsewidth_us(esc_4);    // set the initial pulsewidth to 800 us just to keep them on
   
-    printf("esc1 %s\n",esc_1);
-    printf("esc2 %s\n",esc_2);
-    printf("esc3 %s\n",esc_3);
-    printf("esc4 %s\n",esc_4);
+    printf("esc1: %s , esc2: %s , esc3: %s , esc4: %s \r\n",esc_1 ,esc_2 ,esc_3 ,esc_4);
+    printf("gyro_roll: %e , gyro_roll: %e , gyro_roll: %e \r\n",gyro_roll, gyro_yaw, gyro_pitch);
+    printf("output_r: %e , output_p: %e , output_y: %e \r\n",output_r, output_p, output_y);
 
-  wait_ms(500);
+    wait_ms(500);
+
+    //All the information for controlling the motor's is available.
+    //The refresh rate is 250Hz. That means the esc's need there pulse every 4ms.
+    // while(micros() - loop_timer < 4000);                                      //We wait until 4000us are passed.
+    // loop_timer = micros();                                                    //Set the timer for the next loop.
+
+    // PORTD |= B11110000;                                                       //Set digital outputs 4,5,6 and 7 high.
+    // timer_channel_1 = esc_1 + loop_timer;                                     //Calculate the time of the faling edge of the esc-1 pulse.
+    // timer_channel_2 = esc_2 + loop_timer;                                     //Calculate the time of the faling edge of the esc-2 pulse.
+    // timer_channel_3 = esc_3 + loop_timer;                                     //Calculate the time of the faling edge of the esc-3 pulse.
+    // timer_channel_4 = esc_4 + loop_timer;                                     //Calculate the time of the faling edge of the esc-4 pulse.
+
+    // while(PORTD >= 16){                                                       //Stay in this loop until output 4,5,6 and 7 are low.
+    // esc_loop_timer = micros();                                              //Read the current time.
+    //     if(timer_channel_1 <= esc_loop_timer)PORTD &= B11101111;                //Set digital output 4 to low if the time is expired.
+    //     if(timer_channel_2 <= esc_loop_timer)PORTD &= B11011111;                //Set digital output 5 to low if the time is expired.
+    //     if(timer_channel_3 <= esc_loop_timer)PORTD &= B10111111;                //Set digital output 6 to low if the time is expired.
+    //     if(timer_channel_4 <= esc_loop_timer)PORTD &= B01111111;                //Set digital output 7 to low if the time is expired.
+    // }
+
+
+    
 }
 
  
